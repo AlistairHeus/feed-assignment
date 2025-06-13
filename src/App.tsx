43ce9eb4ef91c27e6f78ca1a70b872e40af77d97
AlistairHeus <kartikjha13@gmail.com>
2025-06-13 14:28:53 +0530

    hotfix: use auth build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AuthProvider, useAuth } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
+import { useAuth } from "./hooks/useAuth";
 
 const AuthTest: React.FC = () => {
   const {
